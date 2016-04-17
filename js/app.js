f1d860e4ee4e3b8150e78c0c9d9452f55b630afa
lksevans12 <lksevans12@gmail.com>
2016-04-17 16:20:44 -0400

    index page done

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$( document ).ready(function() {
+
+	$("#mute-button").on("click",function(){
+			var vid = document.getElementById("myVideo");
+			vid.muted = true;
+			$(this).hide();
+			$("#volume-button").css('display', 'block');
+	});
+
+	$("#volume-button").on("click", function(){
+		var vid = document.getElementById("myVideo");
+			vid.muted = false;
+			$(this).hide();
+			$("#mute-button").css('display', 'block');
+	})
+
+	var mainbottom = $('#services-content').offset().top + $('#services-content').height();
+
+// on scroll, 
+	$(window).on('scroll',function(){
+
+    // we round here to reduce a little workload
+    stop = Math.round($(window).scrollTop());
+    if (stop > mainbottom) {
+        $('.navbar').addClass('past-main');
+    } else {
+        $('.navbar').removeClass('past-main');
+   	}
+	});
+});
