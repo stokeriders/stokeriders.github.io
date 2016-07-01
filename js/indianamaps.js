826d62490fb7d8bebea38fd3a308a9a2d72f5579
lksevans12 <lksevans12@gmail.com>
2016-06-30 20:09:03 -0400

    add miller beach

--- js/indianamaps.js
+++ js/indianamaps.js
@@ -1,5 +1,7 @@
 function initMap() {
 
+	//casino
+
 	var autocasino = new google.maps.places.Autocomplete(
 		(document.getElementById('autocomplete-casino')), {
 			types: ['geocode']
@@ -27,6 +29,37 @@ function initMap() {
 	marker.addListener('click', function() {
 		casinomap.setZoom(15);
 		casinomap.setCenter(marker.getPosition());
+	});
+
+	//miller
+
+		var automiller = new google.maps.places.Autocomplete(
+		(document.getElementById('autocomplete-miller')), {
+			types: ['geocode']
+		});
+
+	var millerdiv = document.getElementById('miller-map');
+	var miller = {lat: 41.6199104, lng: -87.2700966}
+
+	var millermap = new google.maps.Map(millerdiv, {
+		center: miller,
+		zoom: 10
+	});
+
+	var marker = new google.maps.Marker({
+		position: miller,
+		map: millermap
+	});
+
+	millermap.addListener('center_changed', function() {
+		window.setTimeout(function() {
+			millermap.panTo(marker.getPosition());
+		}, 5000);
+	});
+
+	marker.addListener('click', function() {
+		millermap.setZoom(15);
+		millermap.setCenter(marker.getPosition());
 	});
 
 }
