f44fa3e70818f0a0a35b11a369d6c4eba0e8d42c
lksevans12 <lksevans12@gmail.com>
2016-06-30 21:11:22 -0400

    Add zion

--- js/chicagomaps.js
+++ js/chicagomaps.js
@@ -31,4 +31,35 @@ function initMap() {
 		montrosemap.setCenter(marker.getPosition());
 	});
 
+	//Zion
+
+			var autozion = new google.maps.places.Autocomplete(
+		(document.getElementById('autocomplete-zion')), {
+			types: ['geocode']
+		});
+
+	var ziondiv = document.getElementById('zion-map');
+	var zion = {lat: 42.4302502, lng: -87.8168637}
+
+	var zionmap = new google.maps.Map(ziondiv, {
+		center: zion,
+		zoom: 10
+	});
+
+	var marker = new google.maps.Marker({
+		position: zion,
+		map: zionmap
+	});
+
+	zionmap.addListener('center_changed', function() {
+		window.setTimeout(function() {
+			zionmap.panTo(marker.getPosition());
+		}, 5000);
+	});
+
+	marker.addListener('click', function() {
+		zionmap.setZoom(15);
+		zionmap.setCenter(marker.getPosition());
+	});
+
 }
